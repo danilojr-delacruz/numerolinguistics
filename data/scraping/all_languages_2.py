58ca7d7d0ef28351714dc087986603b33e80fcf7
Julian Yu <julian.yu19@imperial.ac.uk>
2021-06-18 18:47:12 +0100

    add a ton of languages

---
+++ data/scraping/all_languages_2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from unidecode import unidecode
+
+
+
+for language in ['ainu', 'alamblak', 'arabic', 'assyrian', 'aymara', 'basque',
+                 'estonian', 'chinook_wawa', 'croatian', 'cuzco_quechua',
+                 'esperanto', 'ganda', 'georgian', 'hawaiian', 'hindi',
+                 'huli', 'hungarian', 'indonesian', 'javanese', 'kiribati',
+                 'maltese', 'manx', 'nahuatl', 'ndom', 'norwegian', 'ojibwa',
+                 'romanian', 'swahili', 'thai', 'vietnamese', 'wolof',
+                 'yoruba', 'zulu']:
+    print(language)
+
+    url = f"http://www.sf.airnet.ne.jp/~ts/language/number/{language}.html"
+    table = pd.read_html(url, match='Reading')[0]
+
+    list_of_nums = table.iloc[:,1].fillna('?').to_list()
+
+    f = open(f'{language.lower()}.txt', 'w')
+    f.write('\n'.join([unidecode(n) for n in list_of_nums]))
+    f.close()
