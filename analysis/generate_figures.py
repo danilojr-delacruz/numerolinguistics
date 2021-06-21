dba26e2a8f0b30e27ebc7a203e37686240d85888
Danilo Jr Dela Cruz <djd19@ic.ac.uk>
2021-06-21 10:29:09 +0100

    Initialise file for figure generation.

---
+++ analysis/generate_figures.py
@@ -0,0 +1,19 @@
+"""Generate the figures used in the report."""
+
+
+def figure_1():
+    pass
+
+
+def figure_2():
+    pass
+
+
+def figure_3():
+    pass
+
+
+if __name__ == "__main__":
+    figures = [figure_1, figure_2, figure_2]
+    for figure in figures:
+        figure()
