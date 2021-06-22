f03eaa82eeca3302a21e2c9fa45481b7630befb7
Danilo Jr Dela Cruz <djd19@ic.ac.uk>
2021-06-22 19:22:43 +0100

    Rename package.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='example',
+    name="numerolinguistics",
     version='1.0',
     packages=find_packages(
         where="numerolinguistics"
