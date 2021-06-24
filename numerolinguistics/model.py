15eb9ecd945171f00f73568d3cf85980a0046eca
Danilo Jr Dela Cruz <djd19@ic.ac.uk>
2021-06-24 13:24:36 +0100

    Implement auxillary model functions.

---
+++ numerolinguistics/model.py
@@ -0,0 +1,25 @@
+"""A collection of functions used to evaluate the number length model."""
+
+
+import numpy as np
+
+
+from scipy.optimize import bisect
+
+
+def ul(n, m):
+    return m * (np.log10(n) + 1)
+
+
+def minimal_N(m, upper=None):
+    """Return the minimal value of N given m via bisection."""
+    lower = m / np.log(10)
+    if upper is None:
+        upper = 10 * m
+
+    try:
+        N  = bisect(lambda n: ul(n, m) - n, lower, upper)
+        return N
+    except ValueError as error:
+        print(error)
+        print(f"Upper limit of {upper} is insufficient.")
